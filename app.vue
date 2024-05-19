2128e4dd1384b78d3cd65ff5c357c4df17060422
blogcreator6109 <blogcreator6109@gmail.com>
2024-05-20 06:31:29 +0900

    썸네일 수정

--- app.vue
+++ app.vue
@@ -6,13 +6,13 @@
     <div class="video-list">
       <Video
         src="https://file.notion.so/f/f/170ef98a-54c7-420e-9e67-c7e5378944ff/adffa2e3-aee8-4fc8-b4be-a3986cbcb5b5/%E1%84%82%E1%85%AC.mp4?id=de9ce384-28fa-44d4-9ef2-d7a9141a190e&table=block&spaceId=170ef98a-54c7-420e-9e67-c7e5378944ff&expirationTimestamp=1716199200000&signature=3DHEt13mzApblDFVCzZb8Te0t-nC24_qQFrbQTulYZA&downloadName=%E1%84%82%E1%85%AC.mp4"
-        thumbnail="https://file.notion.so/f/f/170ef98a-54c7-420e-9e67-c7e5378944ff/56dbf488-ecf0-4791-9746-a99398772558/main1.png?id=3ea6af1c-0335-47d4-a551-1b171c5b156b&table=block&spaceId=170ef98a-54c7-420e-9e67-c7e5378944ff&expirationTimestamp=1716199200000&signature=rqswRARMlGc1UQSuYlr3vMBV6y_tBZUX2cEWk_xbZP8&downloadName=main1.png"
+        thumbnail="https://file.notion.so/f/f/170ef98a-54c7-420e-9e67-c7e5378944ff/7b95fb8c-29e0-4cef-91c4-9d32212e36ac/%E1%84%82%E1%85%B2%E1%84%85%E1%85%A9%E1%84%91%E1%85%B5%E1%86%BA_%E1%84%8A%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF.png?id=6d87eb74-baf8-4942-9bb1-4dc9998af65f&table=block&spaceId=170ef98a-54c7-420e-9e67-c7e5378944ff&expirationTimestamp=1716242400000&signature=ShcXJ11e9o0HwTua0gQZj742MFYY1_zrMgTIK8Z2C78&downloadName=%E1%84%82%E1%85%B2%E1%84%85%E1%85%A9%E1%84%91%E1%85%B5%E1%86%BA_%E1%84%8A%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF.png"
         class="wide"
         ref="video"
       />
       <Video
         src="https://file.notion.so/f/f/170ef98a-54c7-420e-9e67-c7e5378944ff/0378847d-e662-4b31-9de7-6285ec290782/DB.mp4?id=cfb46702-37ed-4360-82be-53ee65569835&table=block&spaceId=170ef98a-54c7-420e-9e67-c7e5378944ff&expirationTimestamp=1716199200000&signature=mla82KUVO5hegrw9uVJGB4Yuw88BQk-q0CmQ2hC6FMc&downloadName=DB.mp4"
-        thumbnail="https://file.notion.so/f/f/170ef98a-54c7-420e-9e67-c7e5378944ff/740294a1-2d63-4ef3-89d9-0a12ed2d5244/main2.png?id=fd3a6846-6129-4acb-9104-5e20477189c4&table=block&spaceId=170ef98a-54c7-420e-9e67-c7e5378944ff&expirationTimestamp=1716199200000&signature=RmKXDsiP2X3P4H4_n9bhe3ATWay5_aF5MqJmfz5lgsY&downloadName=main2.png"
+        thumbnail="https://file.notion.so/f/f/170ef98a-54c7-420e-9e67-c7e5378944ff/fab348b2-9531-4e12-b8f7-66ab3042d678/DB_%E1%84%8A%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF.png?id=6baa3b3e-7f0e-441e-9f53-b535f006efd7&table=block&spaceId=170ef98a-54c7-420e-9e67-c7e5378944ff&expirationTimestamp=1716242400000&signature=zeUcqko2ONVcXvnrcf5hq3MObtf477v3posurke9Bms&downloadName=DB_%E1%84%8A%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF.png"
         class="wide"
         ref="video"
       />
